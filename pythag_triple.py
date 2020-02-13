0a7f2cebe7a2f46dd7f83bc32178787752fff6bf
salt-die <naclmolecule@gmail.com>
2020-02-12 18:55:01 -0600

    optimization found -- the factors of a n**2 are just the factors of n repeated twice, we use this to improve factorint

--- pythag_triple.py
+++ pythag_triple.py
@@ -7,15 +7,18 @@ from collections import Counter
 from random import randint, shuffle
 from math import prod
 
-def random_factoring(n):
+def random_factoring(n): # Finds a factoring of n**2/2
     factors = factorint(n)
     factors = Counter(factors)
-    factors = list(factors.elements())
+
+    factors = list(factors.elements()) * 2 # n**2
+    factors.remove(2)                      # n/2
+
     shuffle(factors)
     split = randint(1, len(factors))
     return prod(factors[:split]), prod(factors[split:])
 
 def gen_triple(n):
     r = 2 * n
-    s, t = random_factoring(r**2//2)
+    s, t = random_factoring(r)
     return r + s, r + t, r + s + t
