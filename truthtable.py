43e541a5f76782238dc5029b62a3a606aacea309
salt-die <naclmolecule@gmail.com>
2020-03-03 19:43:49 -0600

    simplified xor lambda in OP_DICT

--- truthtable.py
+++ truthtable.py
@@ -40,7 +40,7 @@ OP_DICT = {'and': lambda p, q: p and q,
             'or': lambda p, q: p or q,
             '->': lambda p, q: not p or q,
            '<->': lambda p, q: p == q,
-           'xor': lambda p, q: (p and not q) or (not p and q)}
+           'xor': lambda p, q: p != q}
 
 TOKENS = set(OP_DICT).union('~()')
 
