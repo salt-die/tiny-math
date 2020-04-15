49c512cead18da45bf1281bdb336f1fdeadd45d9
salt-die <naclmolecule@gmail.com>
2020-04-15 02:37:32 -0500

    pepify

--- truthtables/parser_lexer.py
+++ truthtables/parser_lexer.py
@@ -1,47 +1,96 @@
 from sly import Lexer, Parser
-from expressions import TRUE as _TRUE, FALSE as _FALSE, Var, Negate, And, Or, Implies, Iff, Xor
+from expressions import (
+    Xor,
+    Or,
+    Iff,
+    Implies,
+    And,
+    Negate,
+    TRUE as TRUE_,
+    FALSE as FALSE_,
+    Var,
+)
 
 
 class LogicLexer(Lexer):
-    tokens = {NAME, IFF, IMPLIES, AND, XOR, OR, TRUE, FALSE}
+    tokens = { NAME, IFF, IMPLIES, AND, XOR, OR, TRUE, FALSE }
     ignore = ' '
     literals = { '~', '(', ')' }
-    IFF = r'<->'; IMPLIES = r'->'; AND = r'and'; XOR = r'xor'; OR = r'or'; NAME = r'[a-z]'
+
+    XOR = r'xor'
+    OR = r'or'
+    IFF = r'<->'
+    IMPLIES = r'->'
+    AND = r'and'
+    NAME = r'[a-z]'
 
     @_(r'T')
-    def TRUE(self, t): t.value = _TRUE; return t
+    def TRUE(self, t):
+        t.value = TRUE_
+        return t
 
     @_(r'F')
-    def FALSE(self, t): t.value = _FALSE; return t
+    def FALSE(self, t):
+        t.value = FALSE_
+        return t
 
     @_(r'[a-z]')
-    def NAME(self, t): t.value = Var(t.value); return t
+    def NAME(self, t):
+        t.value = Var(t.value)
+        return t
 
 
 class LogicParser(Parser):
     tokens = LogicLexer.tokens
-    precedence = (('left', OR, XOR, IFF, IMPLIES), ('left', AND), ('left', '(', ')'), ('right', '~'))
-    lookup = {'or': Or, 'xor': Xor, '<->': Iff, '->': Implies, 'and': And}
 
-    def __init__(self): self.vars = set()
+    precedence = (
+        ('left', XOR, OR, IFF, IMPLIES),
+        ('left', AND),
+        ('left', '(', ')'),
+        ('right', '~')
+    )
+
+    lookup = {
+        'xor': Xor,
+        'or': Or,
+        '<->': Iff,
+        '->': Implies,
+        'and': And
+    }
+
+    def __init__(self):
+        self.vars = set()
 
     @_('expr')
-    def statement(self, p): return p.expr
+    def statement(self, p):
+        return p.expr
 
-    @_('expr OR expr', 'expr XOR expr', 'expr IFF expr', 'expr IMPLIES expr', 'expr AND expr')
-    def expr(self, p): return self.lookup[p[1]](p.expr0, p.expr1)
+    @_('expr XOR expr',
+       'expr OR expr',
+       'expr IFF expr',
+       'expr IMPLIES expr',
+       'expr AND expr',
+    )
+    def expr(self, p):
+        return self.lookup[p[1]](p.expr0, p.expr1)
 
     @_('"(" expr ")"')
-    def expr(self, p): return p.expr
+    def expr(self, p):
+        return p.expr
 
     @_('"~" expr')
-    def expr(self, p): return Negate(p.expr)
+    def expr(self, p):
+        return Negate(p.expr)
 
     @_('TRUE')
-    def expr(self, p): return p.TRUE
+    def expr(self, p):
+        return p.TRUE
 
     @_('FALSE')
-    def expr(self, p): return p.FALSE
+    def expr(self, p):
+        return p.FALSE
 
     @_('NAME')
-    def expr(self, p): self.vars.add(p.NAME.name); return p.NAME
+    def expr(self, p):
+        self.vars.add(p.NAME.name)
+        return p.NAME
