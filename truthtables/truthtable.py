a32fdbb3d5182f40eaea5bcf044932db9fa86547
salt-die <naclmolecule@gmail.com>
2020-04-15 01:54:58 -0500

    moved table_maker to its own file

--- truthtables/truthtable.py
+++ truthtables/truthtable.py
@@ -46,6 +46,7 @@ Operator precedence is parens, negate, then left-to-right.
 from functools import reduce
 from itertools import filterfalse, product
 
+from table_maker import table_maker
 from expressions import TRUE, FALSE, Var, Negate, And, Or, Implies, Iff, Xor
 
 
@@ -107,27 +108,6 @@ def expr_to_tree(expression):
             expression[i] = Var(token)
 
     return build_tree(expression)
-
-def table_maker(*rows):
-    """Generates an aligned table. Modified from https://github.com/salt-die/Table-Maker"""
-    rows = list(rows)
-
-    # Pad the length of items in each column
-    lengths = tuple(map(len, rows[0]))
-    for i, row in enumerate(rows):
-        for j, (item, length) in enumerate(zip(row, lengths)):
-            rows[i][j] = f'{item:^{length}}'
-
-    # Make separators
-    horizontals = tuple("─" * (length + 2) for length in lengths)
-    top, title, bottom = (f'{l}{m.join(horizontals)}{r}' for l, m, r in ('┌┬┐', '├┼┤', '└┴┘'))
-
-    table = [f'│ {" │ ".join(row)} │' for row in rows]
-    table.insert(0, top)
-    table.insert(2, title)
-    table.append(bottom)
-    table = '\n'.join(table)
-    return table
 
 
 class TruthTable:
